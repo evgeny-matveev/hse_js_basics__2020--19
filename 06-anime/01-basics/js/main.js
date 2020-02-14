7dad89918d8720ab5e6452bc5b17d780bf55ce20
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-14 16:32:02 +0300

    add props example

--- 06-anime/01-basics/js/main.js
+++ 06-anime/01-basics/js/main.js
@@ -5,3 +5,23 @@ let targsAnimation01 = anime({
 })
 let targsBtn01 = document.getElementById('targsBtn01')
 targsBtn01.onclick = targsAnimation01.play
+
+let propsAnim01 = anime({
+  targets: '.props .box',
+  translateX: 500,
+  rotate: 180,
+  backgroundColor: '#00ffff',
+  borderRadius: [0, '50%'],
+  easing: 'easeInOutQuad',
+  autoplay: false
+})
+let propsBtn01 = document.getElementById('propsBtn01')
+propsBtn01.onclick = propsAnim01.play
+
+
+
+
+
+
+
+//
