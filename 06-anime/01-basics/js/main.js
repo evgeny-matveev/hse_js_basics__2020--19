d6e3b8a3afafdd5a57e48fba9d76ee2732532c4e
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-14 17:42:47 +0300

    add svg anim

--- 06-anime/01-basics/js/main.js
+++ 06-anime/01-basics/js/main.js
@@ -6,6 +6,7 @@ let targsAnimation01 = anime({
 let targsBtn01 = document.getElementById('targsBtn01')
 targsBtn01.onclick = targsAnimation01.play
 
+
 let propsAnim01 = anime({
   targets: '.props .box',
   translateX: 500,
@@ -19,6 +20,27 @@ let propsBtn01 = document.getElementById('propsBtn01')
 propsBtn01.onclick = propsAnim01.play
 
 
+let valsAnim01 = anime({
+  targets: '.vals .triangle',
+  translateX: 500,
+  autoplay: false,
+  easing: 'easeInOutQuint',
+  delay: anime.stagger(100),
+  rotate: anime.stagger([-270, 270])
+})
+let valsBtn01 = document.getElementById('valsBtn01')
+valsBtn01.onclick = valsAnim01.play
+
+let path = anime.path('path')
+let svgMotion = anime({
+  targets: '.svgAnim .triangle',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 15000,
+  loop: true
+})
 
 
 
