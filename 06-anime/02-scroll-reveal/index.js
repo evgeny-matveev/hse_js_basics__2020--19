40edcb4e2f61f4f101b35faf68ef8ff4dbc0205f
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-21 16:49:34 +0300

    wip

--- 06-anime/02-scroll-reveal/index.js
+++ 06-anime/02-scroll-reveal/index.js
@@ -1,9 +1,9 @@
 let controller = new ScrollMagic.Controller()
 
 // EXAMPLE 1
-let box = document.querySelector('.box')
-let boxAnimation = anime({
-  targets: box,
+let box1 = document.querySelector('#box-1')
+let box1Animation = anime({
+  targets: box1,
   translateX: 500,
   rotate: [0, 360],
   duration: 1500,
@@ -13,13 +13,30 @@ let boxAnimation = anime({
 //   'onEnter' (1), 'onCenter' (0.5), 'onLeave' (0)
 //   или числом от 1 до 0
 new ScrollMagic.Scene({
-  triggerElement: box
+  triggerElement: box1
 })
 .addTo(controller)
-.on('enter', function() {
-  boxAnimation.play()
-})
+.on('enter', () => box1Animation.play())
 
+// EXAMPLE 2
+let box2 = document.querySelector('#box-2')
+let box2Animation = anime({
+  targets: box2,
+  translateX: '80vw',
+  rotate: '+=5turn',
+  easing: 'linear',
+  autoplay: false
+})
+let section3 = document.querySelector('#section-3')
+new ScrollMagic.Scene({
+  triggerElement: section3,
+  duration: section3.getBoundingClientRect().height
+})
+.addTo(controller)
+.setPin(box2)
+.on('progress', e => {
+  box2Animation.seek(box2Animation.duration * e.progress)
+})
 
 
 
