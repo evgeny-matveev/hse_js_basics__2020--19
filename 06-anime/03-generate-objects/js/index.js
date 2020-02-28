420aecb9708586336bb651ba2e6e86365b833b6e
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 16:02:02 +0300

    fn for mouseenter

--- 06-anime/03-generate-objects/js/index.js
+++ 06-anime/03-generate-objects/js/index.js
@@ -2,10 +2,28 @@ let numberOfRects = 10
 
 for (let i = 0; i < numberOfRects; i += 1) {
   let rect = document.createElement('div')
+
   rect.style.width = `${anime.random(0,500)}px`
   rect.style.height = `${anime.random(0,500)}px`
   rect.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
   rect.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
 
+  rect.onmouseenter = function(e) {
+    e.target.style.width = `${anime.random(0,500)}px`
+    e.target.style.height = `${anime.random(0,500)}px`
+    e.target.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
+    e.target.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
+  }
+
   document.body.append(rect)
 }
+
+
+
+
+
+
+
+
+
+//
