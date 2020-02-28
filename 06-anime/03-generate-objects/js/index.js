ef15aa8c47ae2d7ab3388d5bebe464c248f08e10
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 16:16:50 +0300

    wip

--- 06-anime/03-generate-objects/js/index.js
+++ 06-anime/03-generate-objects/js/index.js
@@ -1,21 +1,29 @@
 let numberOfRects = 10
 
 for (let i = 0; i < numberOfRects; i += 1) {
-  let rect = document.createElement('div')
+  let div = document.createElement('div')
 
-  rect.style.width = `${anime.random(0,500)}px`
-  rect.style.height = `${anime.random(0,500)}px`
-  rect.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
-  rect.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
+  if (anime.random(0,100) % 2 === 0) {
+    let size = `${anime.random(0,500)}px`
+    div.style.width = size
+    div.style.height = size
+    div.style.borderRadius = '100%'
+  } else {
+    div.style.width = `${anime.random(0,500)}px`
+    div.style.height = `${anime.random(0,500)}px`
+  }
 
-  rect.onmouseenter = function(e) {
+  div.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
+  div.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
+
+  div.onmouseenter = function(e) {
     e.target.style.width = `${anime.random(0,500)}px`
     e.target.style.height = `${anime.random(0,500)}px`
     e.target.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
     e.target.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
   }
 
-  document.body.append(rect)
+  document.body.append(div)
 }
 
 
