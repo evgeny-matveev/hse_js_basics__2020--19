ccd69e9db07c3ffd08333f4c0b76223d154207ed
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2020-02-28 16:27:30 +0300

    improve onmouseenter

--- 06-anime/03-generate-objects/js/index.js
+++ 06-anime/03-generate-objects/js/index.js
@@ -17,10 +17,21 @@ for (let i = 0; i < numberOfRects; i += 1) {
   div.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
 
   div.onmouseenter = function(e) {
-    e.target.style.width = `${anime.random(0,500)}px`
-    e.target.style.height = `${anime.random(0,500)}px`
-    e.target.style.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
-    e.target.style.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
+    let elStyle = e.target.style
+    let elWidthNum = Number(elStyle.width.slice(0,-2))
+    let elHeightNum = Number(elStyle.height.slice(0,-2))
+
+    if (elWidthNum === elHeightNum) {
+      let size = `${anime.random(0,500)}px`
+      elStyle.width = size
+      elStyle.height = size
+    } else {
+      elStyle.width = `${anime.random(0,500)}px`
+      elStyle.height = `${anime.random(0,500)}px`
+    }
+
+    elStyle.background = `rgb(${anime.random(0,255)},${anime.random(0,255)},${anime.random(0,255)})`
+    elStyle.transform = `translate(${anime.random(0,100)}vw,${anime.random(0,100)}vh) rotate(${anime.random(0,360)}deg)`
   }
 
   document.body.append(div)
